0145f92afe23b1c5a3c697742ec0f566931918b8
jacob <hanxu.jacobhan@gmail.com>
2019-09-07 23:30:24 +1000

    get a new optimize function to predict

--- ana.py
+++ ana.py
@@ -12,11 +12,18 @@ import time
 import pyjarowinkler
 from pyjarowinkler import distance
 
+'''
+@author: xhhan1@student.unimelb.edu.au
+Use different string match methods to interpret the candidate blend words.
+Evaluate the result from the real blend txt file.
+
+Please forgive me to duplicate some of these methods. 
+'''
 
 
 def has_many_duplicate(word):
-    #return true if word has a sequence of
-    #more than 2 consecutive identical letters and flase otherwise
+    # return true if word has a sequence of
+    # more than 2 consecutive identical letters and flase otherwise
     return re.search("([a-zA-Z])\\1{2,}", word)
 
 
@@ -36,14 +43,12 @@ def longest_common_suffix(w1, w2):
     return lcp
 
 
-def is_common_prefix(w1, w2):
-    return w1[:2] == w2[:2]
-
-
-def ngram_test(w1,w2,n):
+# ngram test method
+def ngram_test(w1, w2, n):
     return NGram.compare(w1, w2, N=n)
 
 
+# clean data from the data file
 def clean_data_set(candidate, dic):
     dic_clean = []
     candidate_clean = []
@@ -70,6 +75,7 @@ def clean_data_set(candidate, dic):
     return candidate_clean, dic_clean
 
 
+# process data from the candidate, dict and blends from the source file.
 def process_date():
     blend = []
     with open("candidates.txt") as a:
@@ -83,17 +89,20 @@ def process_date():
     return candidate, dic, blend
 
 
+# Use jaro-winkler method to interpret the candidate blend words
 def predict_blends_global(test_list, dic_list):
     result = []
     count = 0
     recount = 0
     for t in test_list:
         for d in dic_list:
-            if len(longest_common_prefix(t, d)) >= 2:
-                if distance.get_jaro_distance(t, d) >= 0.5:
+            if t[0] == d[0]:
+                if distance.get_jaro_distance(t, d, winkler=True, scaling=0.1) > 0.9\
+                        and ed.eval(t, d) > 1:
                     count = count + 1
-            elif len(longest_common_suffix(t, d)) >= 2:
-                if distance.get_jaro_distance(t, d) >= 0.5:
+            elif t[-1] == d[-1]:
+                if distance.get_jaro_distance(t, d, winkler=True, scaling=0.1) > 0.9\
+                        and ed.eval(t, d) > 1:
                     recount = recount + 1
 
             if count > 0 and recount > 0:
@@ -105,17 +114,18 @@ def predict_blends_global(test_list, dic_list):
     return result
 
 
+# Use ngram(2,3,4...) method to interpret the candidate blend words
 def predict_blends_ngram(test_list, dic_list, n):
     result = []
     count = 0
     recount = 0
     for t in test_list:
         for d in dic_list:
-            if len(longest_common_prefix(t, d)) >= 2:
-                if ngram_test(t, d, n) > 0.3:
+            if t[0] == d[0]:
+                if ngram_test(t, d, n) > 0.5 and ed.eval(t, d) > 1:
                     count = count + 1
-            elif len(longest_common_suffix(t, d)) >= 2:
-                if ngram_test(t, d, n) > 0.3:
+            elif t[-1] == d[-1]:
+                if ngram_test(t, d, n) > 0.5 and ed.eval(t, d) > 1:
                     recount = recount + 1
 
             if count > 0 and recount > 0:
@@ -127,6 +137,7 @@ def predict_blends_ngram(test_list, dic_list, n):
     return result
 
 
+# calculate the result from the real blend words txt
 def calculate_result(result, blends):
     count = 0
     for word in result:
@@ -139,5 +150,13 @@ candidate, dic, blends = process_date()
 clean_candidate, clean_dic = clean_data_set(candidate, dic)
 
 
-result = predict_blends_global(clean_candidate, clean_dic)
-print(calculate_result(result, blends))
+#result = predict_blends_global(clean_candidate, clean_dic)
+result1 = predict_blends_ngram(clean_candidate, clean_dic, 3)
+#print(result)
+print(result1)
+#print(calculate_result(result, blends))
+print(calculate_result(result1, blends))
+
+
+# ngram > 0.5  0.0135  3gram > 0.5 0.01
+# jw > 0.7 0.011 >0.8 0.0114 > 0.9  0.01224
